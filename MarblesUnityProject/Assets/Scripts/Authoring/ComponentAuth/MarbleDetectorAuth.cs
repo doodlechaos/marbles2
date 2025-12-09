e099d544cf84ef6013542fedc08e1119a952d355
doodlechaos <merobbins5@gmail.com>
2025-12-08 19:04:55 -0600

    Add MarbleDetector functionality with event handling for detected marbles and collision detection settings

--- MarblesUnityProject/Assets/Scripts/Authoring/ComponentAuth/MarbleDetectorAuth.cs
+++ MarblesUnityProject/Assets/Scripts/Authoring/ComponentAuth/MarbleDetectorAuth.cs
@@ -1,8 +1,19 @@
 using System;
 using GameCoreLib;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MarbleDetectorAuth : GameComponentAuth<MarbleDetectorComponent>
 {
-    
+    [Serializable]
+    public class DetectedMarbleEvent : UnityEvent<PlayerMarbleAuth> { }
+
+    [SerializeField]
+    DetectedMarbleEvent OnMarbleDetected;
+
+    public bool CollisionEnterDetection = true;
+    public bool TriggerEnterDetection = true;
+
+    public bool CollisionStayDetection = false;
+    public bool TriggerStayDetection = false;
 }
--- MarblesUnityProject/Assets/Scripts/Authoring/ComponentAuth/MarbleEffectAuth.cs
+++ MarblesUnityProject/Assets/Scripts/Authoring/ComponentAuth/MarbleEffectAuth.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public class MarbleEffectAuth : GameComponentAuth<MarbleEffectComponent>
 {
     public MarbleEffect Effect;
+
+    public void ApplyEffect(PlayerMarbleAuth marble) { }
 }
