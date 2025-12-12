fcb04cdaff4635e100f82236eef6cd100b8b60cd
doodlechaos <merobbins5@gmail.com>
2025-12-11 19:08:49 -0600

    Enhance SpinnerSquarePeg prefab with AutoSpin and RenderPrefabIdentifier components. Update SimpleBattleRoyale1 prefab to include additional object references and modify local position and rotation properties. Add SpinnerSquarePeg to RenderPrefabRegistry for improved prefab management.

--- MarblesUnityProject/Assets/Scripts/Shared/GameCoreLib/GCComponents/AutoSpinComponent.cs
+++ MarblesUnityProject/Assets/Scripts/Shared/GameCoreLib/GCComponents/AutoSpinComponent.cs
@@ -13,8 +13,14 @@ namespace GameCoreLib
 
         public override void Step()
         {
+            Logger.Log($"Stepping AutoSpinComponent for {GCObj.Name}");
             //TODO: Each step, rotate the GCObj this component is attached to by SpinDegreesPerSecond degrees in the x, y, z axes respectively.
             //TODO: Be careful to make sure the rotation correctly synchronizes with the physics simulation if it has a physics body for this GCObj.
+            FP stepSeconds = FP.FromFloat(1 / 60f);
+            FPVector3 stepDegrees = SpinDegreesPerSecond * stepSeconds;
+
+            // Rotate around local axes so physics and children stay in sync with the transform hierarchy
+            Transform?.Rotate(stepDegrees, worldSpace: false);
         }
     }
 }
